3908c5ccddfa729ac5eef683eb275361d765d896
Gary Rashed <grashed@gmail.com>
2019-05-05 21:39:16 -0400

    Making Progress!

--- app.R
+++ app.R
@@ -7,6 +7,7 @@
 #    http://shiny.rstudio.com/
 #
 
+
 library(shiny)
 library(dplyr)
 library(leaflet)
@@ -15,51 +16,102 @@ library(shinydashboard)
 source("./global.R")
 
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-
-    # Application title
-    titlePanel("Meat Consumption over 1990 - 2019."),
-    sidebarLayout(
-        sidebarPanel(
-            selectInput(inputId = "countryId",
-                        label = strong("Country"),
-                        choices = unique(fullData$Country)),
-
-            sliderInput('dateRange', label = "Date", min = as.Date("1990", "%Y"), max = as.Date("2019", "%Y"),
-                        timeFormat = "%Y", value = c(as.Date("1990", "%Y"), as.Date("2019", "%Y")))
+ui <- dashboardPage(
+    dashboardHeader(
+        title = "Meat consumption across the globe."),
+        dashboardSidebar(
+            sidebarUserPanel(
+                name = 'Gary Rashed',
+                subtitle = '',
+                image = 'MEDIUM.png'
             ),
-        
-        mainPanel(
-            tabsetPanel(
-                 tabPanel("Map of Meat Consumption"),
-                 tabPanel("Country Breakdown."),
-                 tabPanel("Year over year breakdown.")
+            sidebarMenu(
+                menuItem(
+                    "Dashboard",
+                    tabName = "dashboard",
+                    icon = icon("dashboard")
+                ),
+                menuItem("Widgets", tabName = "widgets",
+                         icon = icon("th"))
             )
-        )    
+        ),
+    dashboardBody(fluidPage(
+        titlePanel("Meat Consumption over 1990 - 2019."),
+        sidebarLayout(
+            sidebarPanel(
+                selectInput(
+                    inputId = "countryId",
+                    label = strong("Country"),
+                    choices = unique(fullData$Country)
+                ) ,
+                
+                sliderInput(
+                    'dateRange',
+                    label = "Date",
+                    min = as.Date("1990", "%Y"),
+                    max = as.Date("2019", "%Y"),
+                    timeFormat = "%Y",
+                    value = c(as.Date("1990", "%Y"), as.Date("2019", "%Y"))
+                )
+            ),
+            
+            mainPanel(
+                tabsetPanel(
+                    tabPanel("Map of Meat Consumption"),
+                    tabPanel("Country Breakdown."),
+                    tabPanel("Year over year breakdown.")
+                )
+            )
+        )
     )
-
-
-        # sidebarlayout(
-        #     sidebarpanel(
-        #     #     sliderinput("bins",
-        #     #                 a"number of bins:",
-        #     #                 min = 1,
-        #     #                 max = 50,
-        #     #                 value = 30)
-        #      )
-        #     ),
-   
-
-    
+    )
 )
+
+
+# # # Define UI for application that draws a histogram
+# ui <- fluidPage(
+#
+#
+#     # Application title
+#     titlePanel("Meat Consumption over 1990 - 2019."),
+#     sidebarLayout(
+#         sidebarPanel(
+#             selectInput(inputId = "countryId",
+#                         label = strong("Country"),
+#                         choices = unique(fullData$Country)),
+#
+#             sliderInput('dateRange', label = "Date", min = as.Date("1990", "%Y"), max = as.Date("2019", "%Y"),
+#                         timeFormat = "%Y", value = c(as.Date("1990", "%Y"), as.Date("2019", "%Y")))
+#             ),
+#
+#         mainPanel(
+#             tabsetPanel(
+#                  tabPanel("Map of Meat Consumption"),
+#                  tabPanel("Country Breakdown."),
+#                  tabPanel("Year over year breakdown.")
+#             )
+#         )
+#     )
+#
+#
+#         # sidebarlayout(
+#         #     sidebarpanel(
+#         #     #     sliderinput("bins",
+#         #     #                 a"number of bins:",
+#         #     #                 min = 1,
+#         #     #                 max = 50,
+#         #     #                 value = 30)
+#         #      )
+#         #     ),
+#
+#
+#
+# )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-
+    
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
